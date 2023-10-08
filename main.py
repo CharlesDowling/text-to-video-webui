734186c227ccef7d6b86f99e636a8ebd6bacb7d8
CharlesDowling <121833213+CharlesDowling@users.noreply.github.com>
2023-10-08 04:19:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import tempfile
-from contextlib import contextmanager
-from typing import List
-import numpy as np
-import cv2
 
 # Import Ai libraries
 import torch
@@ -19,6 +14,8 @@ Video_Path = None
 #
 saveDirectory = "output/"
 
+slidervalue = 8
+
 #Loads the model
 pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
 
@@ -28,7 +25,7 @@ pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 # Removes cpu reliance
 pipe.enable_model_cpu_offload()
 
-
+#
 pipe.progress_bar
 
 ##########################
@@ -40,28 +37,47 @@ pipe.progress_bar
 with gr.Blocks() as server:
 
     # Generates the dang video
-    def generateVideo(Prompt):        
-        video_frames = pipe(Prompt, num_inference_steps=StepsSlider).frames
+    def generateVideo(Prompt,slider):      
+
+        print(slider)
+
+        # Generates the frames  
+        video_frames = pipe(Prompt, num_inference_steps=slider).frames
         print("Finished Generating")
+
+        #Removes witespace for file name
         Prompt.replace(" ", "_")
+
+        # Exports the video to{directory}{filename}.mp4
         Video_Path = export_to_video(video_frames,r"{directory}{filename}.mp4".format(filename=Prompt,directory=saveDirectory))        
         print("Finished Saving First Video")
+
+        # Converts to fromat gradio can use cand replaces the previously generated file
         os.system("ffmpeg.exe -y -i {directory}{filename}.mp4 -vcodec libx264 {directory}{filename}.mp4".format(filename=Prompt,directory=saveDirectory))
+
+        # Reinitialized video player with video file
         VideoPlayer = gr.Video(Video_Path)
         return VideoPlayer
+    
+    # Changes Label Text
+    def Display_Info(steps):
+        Text_to_Display = ("Seconds: " + str(steps/8))
+        return Text_to_Display
 
     # Prompt input text
     PromptField = gr.TextArea(label="Prompt", placeholder="Type in prompt here")
 
-    # Made a slider for amount of frames. Maximum is an hour because if you really need more...WHY?
-    StepsSlider = gr.Slider(0, 28801, 1, label="Frames", step=8, info="1", interactive=True)
+    with gr.Row():
+        # Made a slider for amount of frames. Maximum is an hour because if you really need more...WHY?
+        SliderSteps = gr.Slider(0, 28801, slidervalue, label="Frames", step=8, info="1", interactive=True)
+        gr.Interface(Display_Info, SliderSteps, gr.Label(), live=True)
+    
 
     # Made a dummy video player, will be replaced by player generated in generateVideo()
     VideoPlayer = gr.Video(Video_Path, interactive=False, height=400)
 
     # Button to generate and click event handler
     GenerateButton = gr.Button("Generate Video")
-    GenerateButton.click(fn=generateVideo, inputs=PromptField, outputs=VideoPlayer, api_name="generate_video")
+    GenerateButton.click(fn=generateVideo, inputs=[PromptField, SliderSteps], outputs=VideoPlayer, api_name="generate_video")
 
-server.launch()
-
+server.launch(debug=True)
