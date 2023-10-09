59aaf25280d2963d3ad977a3ebc54daf47a3195c
CharlesDowling <121833213+CharlesDowling@users.noreply.github.com>
2023-10-09 08:58:43 -0400

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
-call python -m venv ./
-call scripts/activate.bat
-call pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+call python -m venv ./venv
+call /venv/scripts/activate.bat
+call pip3 install torch==2.0.1+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 call pip3 install -r ./requirements.txt
 pause
